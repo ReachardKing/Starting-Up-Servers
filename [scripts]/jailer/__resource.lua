551464f65d7988796a6ff30ae4e005503db55622
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2023-08-15 11:50:50 -0400

    Update __resource.lua

--- [scripts]/jailer/__resource.lua
+++ [scripts]/jailer/__resource.lua
@@ -1,3 +1,10 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_script "clientJailer.lua"
 server_script "serverJailer.lua"
+
+files {
+  'web/Evidence.html',
+  'web/Evidence.js
+}
+
+ui_page 'web/Evidence.html'
