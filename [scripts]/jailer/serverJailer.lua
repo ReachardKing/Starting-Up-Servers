4324121e66e724f0ac631ee93e4cdd4d38b1ba6b
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2023-08-23 16:50:49 -0400

    Update serverJailer.lua

--- [scripts]/jailer/serverJailer.lua
+++ [scripts]/jailer/serverJailer.lua
@@ -1,21 +1,3 @@
 AddEventHandler("Mackenzie_Rich:Jail", function()
-        local JailID = tonumber(KeyboardInput("Spawn any vehicles", 20))
-    if JailID == nil then
-        Notify("Nothing was entered")
-        return
-    end
-end)
 
-function GetJailWaypoint(name, ID)
-end
-
-function KeyboardInput()
-    DisplayOnscreenKeyboard(1, "", "", "", "", "", "", 255)
-    while (UpdateOnscreenKeyboard() == 0)
-      Citizen.Wait(0)
-    end
-    if (GetOnscreenKeyboardResult()) then
-        GetJailWaypoint(GetOnscreenKeyboardResult())
-        Notify('~b~Vehicle Spawned')
-    end
 end
