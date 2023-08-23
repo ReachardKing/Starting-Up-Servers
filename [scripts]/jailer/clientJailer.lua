28cb00169e05956a085349af05c1dc0d361fe73b
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2023-08-23 16:53:12 -0400

    Update clientJailer.lua

--- [scripts]/jailer/clientJailer.lua
+++ [scripts]/jailer/clientJailer.lua
@@ -1,5 +1,5 @@
 AddEventHandler("Mackenzie_Rich:Jail", function()
-        local JailID = tonumber(KeyboardInput("Spawn any vehicles", 20))
+        local JailID = tonumber(KeyboardInput("Want to jail someone?", 20))
     if JailID == nil then
         Notify("Nothing was entered")
         return
