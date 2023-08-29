78ef068474aa40967cb61033de2b6dd5ad99eec9
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2023-08-29 11:12:18 -0400

    Update main.lua

--- [Functions]/Chat Command/server/main.lua
+++ [Functions]/Chat Command/server/main.lua
@@ -3,25 +3,6 @@
 print("Shout out the the person who made refine-chat !!! !!  !!!")
 print("Never give up !! !!")
 
-RegisterNetEvent('_chat:messageEntered', function(author, color, message)
-  if not message or not author then
-      return
-  end
-  TriggerEvent('chat:addMessage', source, author, message)
-end)
-
-RegisterNetEvent('__cfx_internal:commandFallback', function(command)
-  local name = GetPlayerName(source)
-
-  TriggerEvent('chat:addMessage', source, name, '/' .. command)
-
-  if not WasEventCanceled() then
-      TriggerClientEvent('chat:addMessage', -1, name, false, '/' .. command) 
-  end
-
-  CancelEvent()
-end)
-
 RegisterCommand("311", function(source, name, args)
   local src = source
   local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
