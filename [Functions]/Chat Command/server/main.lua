4c2f2015c4df78b8d50fafb15096136dce7b5c21
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2023-08-29 12:09:50 -0400

    Update main.lua

--- [Functions]/Chat Command/server/main.lua
+++ [Functions]/Chat Command/server/main.lua
@@ -3,72 +3,43 @@
 print("Shout out the the person who made refine-chat !!! !!  !!!")
 print("Never give up !! !!")
 
-RegisterCommand("311", function(source, name, args)
-  local src = source
-  local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
-  
-  TriggerClientEvent("chat:addMessage", source, {
-    color = {255, 0,0},
-    multiline = true,
-    args = {'311 '..name.. '' .. '', msg}
-  })
-end)
-  
-RegisterCommand("211", function(source, name, args)
-  local src = source
-  local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
-  
-  TriggerClientEvent("chat:addMessage", source, {
-    color = {255, 0,0},
-    multiline = true,
-    args = {'211 '..name.. '' .. '', msg}
-  })
-end)
-  
-RegisterCommand("ooc", function(source, name, args)
-  local src = source
-  local ped = GetPlayerPed(src)
-  local playerCoords = GetEntityCoords(ped)
-  local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
-  local Players = GetPlayers()
-  
-  TriggerClientEvent("me:client:command", Player, src, msg)   
-  TriggerClientEvent("chat:addMessage", source, {
-    color = {255, 0,0},
-    multiline = true,
-    args = {'ooc'..name.. '' .. '', msg}
-  })
-end)
-  
-RegisterCommand("Pdalert", function(source, name, args)
-  local src = source
-  local ped = GetPlayerPed(src)
-  local playerCoords = GetEntityCoords(ped)
-  local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
-  local Players = GetPlayers()
-  
-  TriggerClientEvent("me:client:command", Player, src, msg)   
-  TriggerClientEvent("chat:addMessage", source, {
-    color = {255, 0,0},
-    multiline = true,
-    args = {'pdalert' .. '', msg}
-  })
+AddEventHandler("chat:addMessage", function(source, args, msg)
+    sm = stringsplit(msg, " ");
+    
+    if sm[1] == "/311" then
+      CancelEvent()
+      TriggerClientEvent("chat:addMessage", source, { color = {255, 0, 0}, multiline = true, args = {'311'..msg}})
+    end
+    if sm[1] == "/211" then
+      CancelEvent()
+      TriggerClientEvent("chat:addMessage", source, { color = {255, 0, 0}, multiline = true, args = {'211'.. msg}})
+    end
+    if sm[1] == "/ooc" then
+      CancelEvent()
+      TriggerClientEvent("chat:addMessage", source, { color = {255, 0, 0}, multiline = true, args = {'ooc'.. msg}})
+    end 
+    if sm[1] == "/Pdalert" then
+      CancelEvent()
+      TriggerClientEvent("chat:addMessage", source, { color = {255, 0, 0}, multiline = true, args = {'Pdalert'.. msg}})
+    end
+    if sm[1] == "/EMSalert" then
+      CancelEvent()
+      TriggerClientEvent("chat:addMessage", source, { color = {255, 0, 0}, multiline = true, args = {'EMSalert'..  msg}})
+    end
 end)
 
-RegisterCommand("EMSalert", function(source, name, args)
-  local src = source
-  local ped = GetPlayerPed(src)
-  local playerCoords = GetEntityCoords(ped)
-  local msg = table.concat(args, ' '):gsub('[~<].-[>~]', '')
-  local Players = GetPlayers()
-
-  TriggerClientEvent("me:client:command", Player, src, msg)   
-  TriggerClientEvent("chat:addMessage", source, {
-    color = {255, 0,0},
-    multiline = true,
-    args = {'EMSalert' .. '', msg}
-  })
-end)
+function stringsplit(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  
+  local t={} ; i=1
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    t[i] = str
+    i = i + 1
+  end
+  return t
+end
 
 RegisterCommand("clear", function(source, args)
   TriggerEvent("chat:clear")
