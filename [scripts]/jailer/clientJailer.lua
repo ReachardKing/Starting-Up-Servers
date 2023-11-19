bdc0021f5e7b22d758bbcfbf6c8c8bfa21ebbd99
ReachardKing <67172940+ReachardKing@users.noreply.github.com>
2023-11-19 11:54:19 -0500

    Update clientJailer.lua

--- [scripts]/jailer/clientJailer.lua
+++ [scripts]/jailer/clientJailer.lua
@@ -1,21 +1,5 @@
 AddEventHandler("Mackenzie_Rich:Jail", function()
-        local JailID = tonumber(KeyboardInput("Want to jail someone?", 20))
-    if JailID == nil then
-        Notify("Nothing was entered")
-        return
-    end
 end)
-
-function KeyboardInput()
-    DisplayOnscreenKeyboard(1, "", "", "", "", "", "", 255)
-    while (UpdateOnscreenKeyboard() == 0)
-      Citizen.Wait(0)
-    end
-    if (GetOnscreenKeyboardResult()) then
-        GetJailWaypoint(GetOnscreenKeyboardResult())
-        Notify('~b~Vehicle Spawned')
-    end
-end
 
 RegisterNUICallback("Submit", function()
         TriggerEvent("Mackenzie_Rich:Jail")
